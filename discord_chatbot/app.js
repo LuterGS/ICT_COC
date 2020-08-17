722587f081d8e84c26c3d9ede06a0fbdbb34696b
junsik1998 <junsik1998@naver.com>
2020-08-17 20:56:29 +0900

    지역 선택 기능

--- discord_chatbot/app.js
+++ discord_chatbot/app.js
@@ -1,26 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const axios = require('axios');
-const { send } = require('process');
 
 real_token = ' ';
 test_token = ' ';
 
+allow = ['서울시', '관악구', '도봉구', '서초구'];
 data = [];
 select = '';
 index = 0;
 user_alert = false;
 today_alert = false;
+alert_select = '관악구';
 
 function getData(input_city, msg){
-  axios.get('http://175.193.68.230/sendData', {
+  axios.post('http://175.193.68.230/sendData', {
     city: input_city
   })
   .then(function (response) {
     data = response.data;
-    msg.reply(' [' + input_city + ' 새소식] ');
-    msg.reply('제목 : ' + data[0].title);
-    msg.reply('내용 : ' + data[0].content);
+    msg.reply(' [' + input_city + ' 새소식] '+ '\n제목 : ' + data[0].title + '\n내용 : ' + data[0].content);
     index = 0;
   })
   .catch(function (error) {
@@ -34,6 +33,7 @@ function send_alert(msg){
     console.log(date.getSeconds());
     if(date.getSeconds() == 19 && !today_alert){
       msg.channel.send('19초 알림 (19시 알림으로 변경 예정)')
+      getData(alert_select, msg);
       today_alert = true;
     }
     if(date.getSeconds() == 23) today_alert = false;
@@ -47,37 +47,56 @@ client.on('ready', () => {
 
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === '서울시 새소식 봇 정보') {
+    msg.reply('ICT COC AI 공모전 - 세줄요약장인 팀 \n방준식 : 챗봇 제작 \n이관석 : 한글 요약 알고리즘 \n이호현 : APP & Server \n윤동근 : API로 받은 정보 처리');
   }
 
   if (msg.content.slice(0, 2) === '알림') {
-    let onoff =  msg.content.split(' ')[1];
-    if(onoff.slice(0, 1) == '켜') {
+    let command =  msg.content.split(' ');
+    if(command[1].slice(0, 1) === '켜') {
       user_alert = true;
       today_alert = false;
       msg.reply('19초 알림을 켭니다. (19시 알림으로 변경 예정)');
+      msg.reply('현재 알림 설정된 지역은 ' + alert_select + '입니다. 지역 변경은 [알림 지역 (지역이름)] 명령으로 가능합니다.');
       setInterval(send_alert.bind(null, msg), 1000);
     }
-    if(onoff.slice(0, 1) == '꺼') {
+    if(command[1].slice(0, 1) === '꺼') {
       user_alert = false;
       msg.reply('알림을 끕니다.');
+    }
+    if(command[1] === '지역'){
+      if(allow.indexOf(command[2]) == -1){
+        msg.reply('서비스되지 않는 지역입니다.');
+      }
+      else{
+        alert_select = command[2];
+        msg.reply('알림 지역을 ' + alert_select + '로 변경했습니다.');
+      }
     }
   }
 
   
   if (msg.content.slice(-3) === '새소식'){
-    select = msg.content.split(' ')[0];
-    getData(select, msg);
+    if(allow.indexOf(msg.content.split(' ')[0]) == -1){
+      msg.reply('서비스되지 않는 지역입니다.');
+    }
+    else{
+      select = msg.content.split(' ')[0];
+      getData(select, msg);
+    }
   }
 
   if (msg.content === '더보기') {
       if(select != ''){
+          let max = data.length;
           for (var i = index; i < index + 3; i++) {
-            msg.reply('날짜 : ' + data[i].date);
-            msg.reply('제목 : ' + data[i].title);
-            msg.reply('내용 : ' + data[i].content.slice(0,1000));
-            msg.reply('__________');
+            if(i <= max){
+              msg.reply('날짜 : ' + data[i].date + '\n제목 : ' + data[i].title + '\n내용 : ' + data[i].content.slice(0,1000) + '\n__________');
+            }
+            else{
+              msg.reply('더 볼 수 있는 소식이 없습니다.');
+              break;
+            }
           }
           index += 3;
       }
