29d9ff719d0e2fc1ca0f8546c4d795857bf30af3
LuterGS <koo04034@gmail.com>
2020-08-17 19:58:21 +0900

    마무리작업 (반복문으로)

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from AI.summarize.summary import line3_summary
 from article.get_articles import get_articles
 from DB.main import query_mysql
+import time
 
-
-def get_onetool(location="gwanak", length=10, db_name="news_2", sentence_tokenizer="kss", decay_factor=0.85, max_iteration_num=15):
+def get_onetool(location="nowon", length=10, db_name="news_2", sentence_tokenizer="kss", decay_factor=0.85, max_iteration_num=15):
     articles = get_articles(location, length)
     for article in articles:
         article.content = line3_summary(article.content, sentence_tokenizer, decay_factor, max_iteration_num)
         print(article.content)
-        # query_mysql(db_name, article.number, article.title, article.content, article.date, article.url)
+        query_mysql(db_name, article.number, article.title, article.content, article.date, article.url)
 
 
 if __name__ == "__main__":
-    get_onetool()
+
+    while True:
+        get_onetool("gwanak", 10, "news_1")
+        get_onetool("dobong", 10, "news_2")
+        get_onetool("seocho", 10, "news_3")
+        time.sleep(3600)
