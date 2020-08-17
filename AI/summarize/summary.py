b9212c7d3fbd979d8b74d70a7d841fa5f513bc06
LuterGS <koo04034@gmail.com>
2020-08-17 19:28:08 +0900

    문장 tokenizer 부분 정교화

--- AI/summarize/summary.py
+++ AI/summarize/summary.py
@@ -5,37 +5,76 @@ import kss
 import re
 
 def line3_summary(text, sentence_tokenizer="re", decay_factor=0.85, max_iteration_num=15):
-    splited_sentence, splited_sentence_num = __get_splited_sentence(text, sentence_tokenizer)
+    native_splited, splited_sentence, splited_sentence_num = __get_splited_sentence(text, sentence_tokenizer)
     keyword = __get_keyword(splited_sentence, decay_factor, max_iteration_num)
-    if keyword == "NULL" == 0:
+    if keyword == "NULL" or len(keyword) == 0:
         print("문장 요약 실패")
-        return text
+        final_result = ""
+        for text in native_splited:
+            final_result += text + "\n"
+        return final_result
     print(keyword)
     result = __get_score(splited_sentence, splited_sentence_num, keyword)
-    final_result = result[0][1] + "\n" + result[1][1] + "\n" + result[2][1]
+    final_result = ""
+    if len(result) < 3:
+        sentence_length = len(result)
+    else:
+        sentence_length = 3
+    for i in range(sentence_length):
+        final_result += result[i][1] + "\n"
     return final_result
 
 
 def __get_splited_sentence(whole_text, sentence_tokenizer="re"):
-
+    """
+    :param whole_text: 기사의 원문
+    :param sentence_tokenizer: 어떤 tokenizer를 사용할 것인지
+        re : . ? ! 3개를 기준으로 분리
+        enter : 엔터로 구분
+        jum : . 하나로 구분
+        kss : kss 토크나이저 사용
+        만약 특수기호가 있을 경우 특수기호 분류 후 kss 사용
+    :return:
+    """
+    print(whole_text)
+    # 처음 입력이 리스트일 경우
     if type(whole_text) == list:
         splited = [normalize(sentence, english=True, number=True) for sentence in whole_text]
         splited_num = len(splited)
         return splited, splited_num
 
+    # 문자열에 특수기호가 있을 경우
+    special_character = ['○','□', '▣', '※', '①', '②', '③', '◇']
+    special_counter = 0
+    for special in special_character:
+        special_counter += whole_text.count(special)
+    if special_counter > 0:
+        special_splited = re.split("[○□▣※◇①②③]", whole_text.replace("\n", "")[:-1])
+        splited = []
+        for sentence in special_splited:
+            sentence = kss.split_sentences(sentence)
+            if type(sentence) == list:
+                for sentence_splited in sentence:
+                    splited.append(sentence_splited)
+            else:
+                splited.append(sentence)
+        sentence_tokenizer = "special_character"
+        print("TEST ", splited)
+
+
     if sentence_tokenizer == "re":
-        splited = re.split("[.!?] ", whole_text.replace("\n", "")[:-1])
+        splited = re.split("[.!?]", whole_text.replace("\n", "")[:-1])
     elif sentence_tokenizer == "kss":
         splited = kss.split_sentences(whole_text.replace("\n", ""))
     elif sentence_tokenizer == "enter":  # 아직 구현중, normalize 부분으로 찾으면 될거같은데...
         splited = whole_text.split("\n")
     elif sentence_tokenizer == "jum":
         splited = whole_text.split(".")
-    splited = [normalize(sentence, english=True, number=True) for sentence in splited]
+    keyword_splited = [normalize(sentence, english=True, number=True) for sentence in splited]
     print(splited)
     splited_num = len(splited)
 
-    return splited, splited_num
+    return splited, keyword_splited, splited_num
 
 def __get_keyword(splited_sentence, decay_factor, max_iteration_num):
     try:
@@ -43,7 +82,7 @@ def __get_keyword(splited_sentence, decay_factor, max_iteration_num):
         decay_factor = decay_factor  # 이 단어가 계속 선호하는 단어인지 (소멸되지 않을 확률), 보통 85%로 잡는다.
         max_iteration_num = max_iteration_num  # 최대 반복횟수
         keyword, _, _ = pick_keyword.extract(splited_sentence, decay_factor, max_iteration_num)  # 키워드 추출
-    except FileNotFoundError:
+    except ValueError:
         keyword = "NULL"
     finally:
         return keyword
