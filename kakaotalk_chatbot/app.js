4fa5ba4e79954da87a4290050c57e88dbdcbb073
junsik1998 <junsik1998@naver.com>
2020-08-19 23:56:42 +0900

    kakaotalk chatbot

---
+++ kakaotalk_chatbot/app.js
@@ -0,0 +1,146 @@
+const http = require('http');
+const express = require('express');
+const axios = require('axios');
+
+var app = express();
+app.use(express.json());
+
+
+const port = 8080;
+
+var allow = ['관악구', '도봉구', '서초구']
+var data = [];
+var index = 1;
+
+
+function getData(input_city, res){
+  axios.post('http://175.193.68.230/sendData', {
+    city: input_city
+  })
+  .then(function (response) {
+    data = response.data;
+    index = 1;
+    res.send({
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            simpleText: {
+              text: "[" + input_city + " 새소식]\n" + '제목 : ' + data[0].title + '\n내용 : ' + data[0].content
+            }
+          }
+        ]
+      }
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+app.post("/news", function(req, res) {
+  var city = req.body.action.params.city;
+  if(allow.indexOf(city) !== -1){
+    getData(city, res);
+  }
+  else{
+    res.send({
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            simpleText: {
+              text: city + "는 서비스 준비중인 지역입니다. \n현재 서비스 가능 지역은 관악구, 도봉구, 서초구 입니다."
+            }
+          }
+        ]
+      }
+    });
+  }
+    
+});
+
+app.post("/more", function(req, res){
+  let max = data.length;
+  if(max !== 0){
+    if(index+2 < max){
+      res.send({
+        version: "2.0",
+        template: {
+          outputs: [
+            {
+              listCard: {
+                header: {
+                  title: "새소식 더보기"
+                },
+                items: [
+                  {
+                    title: data[index].title,
+                    description: data[index].content,
+                    link: {
+                      web: data[index].url
+                    }
+                  },
+                  {
+                    title: data[index+1].title,
+                    description: data[index+1].content,
+                    link: {
+                      web: data[index+1].url
+                    }
+                  },
+                  {
+                    title: data[index+2].title,
+                    description: data[index+2].content,
+                    link: {
+                      web: data[index+2].url
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      })
+      index += 3;
+    }
+    else{
+      res.send({
+        version: "2.0",
+        template: {
+          outputs: [
+            {
+              simpleText: {
+                text: "더 이상 볼 수 있는 소식이 없습니다."
+              }
+            }
+          ]
+        }
+      });
+    }
+  }
+  else{
+    res.send({
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            simpleText: {
+              text: "00구 새소식 명령으로 지역을 먼저 선택해 주세요."
+            }
+          }
+        ]
+      }
+    });
+  }
+  
+})
+
+app.listen(port, function() {
+	console.log("server start");
+});
+
+
+app.get('/', (req, res) => {
+  res.send('서울시 새소식 카카오톡 챗봇 API 서버');
+})
+
