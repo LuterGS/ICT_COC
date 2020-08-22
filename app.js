be56cd278fbd2d8773673851df78e88d2bc4a163
Randomshot <sksghgus7175@google.com>
2020-08-22 18:16:08 +0900

    optimization

--- app.js
+++ app.js
@@ -15,94 +15,44 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, '/public')));
 mysql.connect();
 
-function removeTag(content) {
-
-    content = content.replace(/(<([^>]+)>)/ig, "");
-    content = content.replace(/&nbsp;/gi, "");
-    content = content.replace(/&gt;/gi, "");
-    content = content.replace(/○/gi, "");
-    content = content.replace(/\n/gi, "");
-    content = content.replace(/\./gi, "");
-
-    return content;
-}
-
-function getData(){
-    const xmlurl = 'http://openapi.seoul.go.kr:8088/7a51596966736b733330767a6e5975/xml/GwanakNewsList/1/100/'
-    request.get(xmlurl, function(err, res, body) {
-        if (err) console.log(err);
-        else {
-            if (res.statusCode == 200) {
-                var result = body;
-                var xmlToJson = convert.xml2json(result, {
-                    compact: true,
-                    spaces: 4
-                })
-                var stationCheck = -1;
-                var parseJson = JSON.parse(xmlToJson);
-                var value = parseJson.GwanakNewsList;
-                var title;
-                var date;
-                var content;
-                var number;
-                var url;
-                mysql.query('SELECT number FROM news ORDER BY number DESC LIMIT 1', function(err, result) {
-                    if (err) console.log(err);
-                    else {
-
-                        for (var i = 0; i < 100; i++) {
-
-                            title = value.row[i].TITLE._text;
-                            date = value.row[i].WRITEDAY._text;
-                            content = value.row[i].CONTENT._text;
-                            number = value.row[i].SEQ._text;
-                            content = removeTag(content);
-                            date = date.substring(0, 8);
-                            url = value.row[i].EXPANDED_URL._text;
-                            if (result[0].number == number) {
-                                console.log("not updated");
-                                break;
-
-                            } else {
-                                mysql.query('INSERT INTO news (number,title,content,date,url) VALUES (?,?,?,?,?)', [number, title, content, date,url], function(err, result) {
-                                    if (err) console.log(err);
-                                    else {}
-                                });
-                            }
-                        }
-                    }
-                });
-
-            }
-        }
-    });
-}
-
-app.get('/getData', function(req, res) {
-    getData();
-});
-
 app.post('/sendData',function(req,res){
 
     var city = req.body.city;
     var sql;
     console.log(city);
     if(city == '관악구'){
-         sql = 'SELECT * FROM news_1 ORDER BY number DESC LIMIT 10';
+         sql = 'SELECT * FROM gwanak ORDER BY number DESC LIMIT 10';
+    }
+    else if(city == '강동구'){
+        sql = 'SELECT * FROM gangdong ORDER BY number DESC LIMIT 10';
+    }
+    else if(city == '동대문구'){
+        sql = 'SELECT * FROM dongdaemun ORDER BY number DESC LIMIT 10';
     }
     else if(city == '도봉구'){
-        sql = 'SELECT * FROM news_2 ORDER BY number DESC LIMIT 10';
+        sql = 'SELECT * FROM dobong  ORDER BY number DESC LIMIT 10';
     }
     else if(city == '서초구'){
-    
-        sql = 'SELECT * FROM news_3 ORDER BY number DESC LIMIT 10';
+        sql = 'SELECT * FROM seocho ORDER BY number DESC LIMIT 10';
     }
+    else if(city == '송파구'){
+        sql = 'SELECT * FROM songpa  ORDER BY number DESC LIMIT 10';
+    }
+    else if(city == '동작구'){
+        sql = 'SELECT * FROM dongjak ORDER BY number DESC LIMIT 10';
+    }
+    else if(city == '서대문구'){
+        sql = 'SELECT * FROM seodaemun ORDER BY number DESC LIMIT 10';
+    }
+    else if(city == '양천구'){
+        sql = 'SELECT * FROM yangchun  ORDER BY number DESC LIMIT 10';
+    }
+
 
     mysql.query(sql,function(err,result){
         if(err) console.log(err);
         else{
            console.log("send Data!");
-           console.log(result);
            res.send(result);
         }
     });
